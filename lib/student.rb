4f328a6c9123cbede8f87e9f9eeab28306f1a781
Alex Darvin <amdarvin14@gmail.com>
2020-10-10 15:14:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,10 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
-  def initialize(name, grade)
+  def initialize(name, grade, id)
+    @name = name
+    @grade = grade
+    @id = id
     
   
 end
